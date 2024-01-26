eb3485985f6e99073a5bfd033bbf9ad76dc9e3e7
iulikqa <iulikqa@gmail.com>
2024-01-26 22:17:15 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Task2.java

---
+++ src/HW2.java
@@ -0,0 +1,23 @@
+public class HW2 {
+    public static void main(String[] args) {
+        /*Необходимо создать целочисленные переменные a и b, присвоить произвольные значения переменным на ваш выбор и
+        вывести результаты следующих операций с этими переменными:
+            сложение;
+            умножение;
+            вычитание;
+            деление;
+            остаток от деления.
+        Так же сделать проверку на четность этих переменных и вывести результат.
+        */
+
+        int a = 98;
+        int b = 49;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a % 2);
+        System.out.println(b % 2);
+    }
+}
