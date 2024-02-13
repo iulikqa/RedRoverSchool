101a0d14c515661826cb82b9f221a3294854057d
iulikqa <iulikqa@gmail.com>
2024-02-13 21:33:26 +0200

    HW5_1 & HW5_2

--- src/HW_CrazyTesters/Less_3_to_5/Task10_hollowPyramid.java
+++ src/HW_CrazyTesters/Less_3_to_5/Task10_hollowPyramid.java
@@ -3,7 +3,7 @@ package HW_CrazyTesters.Less_3_to_5;
 public class Task10_hollowPyramid {
     public static void main(String[] args) {
 //  Напишите программу, которая выводит на экран полую пирамиду из чисел высотой 6 рядов:
-//         1        1
+//         1
 //       2  3
 //      4    5
 //     6      7
@@ -11,5 +11,47 @@ public class Task10_hollowPyramid {
 //  10    11   12
 
 
+        int height = 1;
+        int step = 0;
+
+        // Вывод пустой пирамиды
+        System.out.println("Пустая пирамида из чисел:");
+
+//      Основной внешний цикл, который отвечает за количество строк в пирамиде.
+        for (int i = 0; i < 5; i++) {
+            // Вывод пробелов перед числами
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print(" ");
+            }
+
+//          Внутренний цикл - выводит
+//          пробелы перед числами, чтобы создать пирамидальную форму.
+            for (int j = 0; j < 1; j++) {
+                System.out.print(height);
+                height += 1;
+            }
+
+            for ( int j = 0; j < i; j++){
+                System.out.print("  ");
+            }
+
+            if (i == 0){
+                System.out.println();
+                continue;
+            }else {
+                for (int j = 0; j < 1; j++){
+                    System.out.print(height);
+                    height += 1;
+                }
+            }
+            System.out.println();
+            step++;
+        }
+        if (step == 4){
+            for (int j = 0; j < 3; j++){
+                System.out.print(height + "   ");
+                height++;
+            }
+        }
     }
 }
--- src/HW_CrazyTesters/Less_3_to_5/Task9_evenOutTheOddsZeroingEvenElementsInAnArray.java
+++ src/HW_CrazyTesters/Less_3_to_5/Task9_evenOutTheOddsZeroingEvenElementsInAnArray.java
@@ -1,11 +1,33 @@
 package HW_CrazyTesters.Less_3_to_5;
 
+import java.util.Arrays;
+
 public class Task9_evenOutTheOddsZeroingEvenElementsInAnArray {
     public static void main(String[] args) {
 // Напишите программу, которая создает массив из 10 целых чисел,
 // инициализирует его числами от 1 до 10. Затем программа должна найти
 // все четные числа и заменить их на ноль. В конце программа выводит измененный массив.
 
+//      Создание массива из 10 целых чисел
+        int[] arr = new int[10];
 
+//      Цикл для инициализация массива числами от 1 до 10
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+//      Цикл для поиска и замены на 0 всех четных чисел
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] = 0;
+            }
+        }
+
+//      Цикл для вывода измененного массива
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
